5c79b620f3ce614feff71c36a69b62a60e0b212c
Junaid Afzal <junaidafzal7274@gmail.com>
2021-09-16 21:03:32 +0500

    logging,

--- changingDB/cacheTest/views.py
+++ changingDB/cacheTest/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.core.cache import cache as radis_cache
+from django.core.cache import cache 
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 
@@ -10,14 +10,14 @@ def cache_test(request):
 
     ip = str(request.META.get('REMOTE_ADDR'))
 
-    radis_cache.set(ip,'2',timeout=0)
+    cache.set(ip,'2',timeout=22)
     print(ip)
-    print(radis_cache.get(ip))
+    print(cache.get(ip))
 
     #print(radis_cache.get(1))
 
 
-    return HttpResponse(str(radis_cache.get('objects')))
+    return HttpResponse(str(cache.keys('*')))
 
 
 
